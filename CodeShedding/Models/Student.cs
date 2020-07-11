96567a3f1712a97eda833b2130d6423689ace5f9
Precedence <precedencemthethwaphiwe@gmail.com>
2020-07-11 00:59:30 -0700

    added a Student Class and Implemented a connection to azure Cosmos BD, also implement Create method for testing purposes

---
+++ CodeShedding/Models/Student.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeShedding.Models
+{
+    public class Student
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Student Number")]
+        public string Student_Number { get; set; }
+
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string First_Name { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Last_Name { get; set; }
+
+
+        [Display(Name = "Email address")]
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Display(Name = "Home Address")]
+        [Required]
+        public string Home_Address { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Contact Number")]
+        [Required()]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",
+                   ErrorMessage = "Entered Contact number format is not valid.")]
+        public string Mobile { get; set; }
+
+        [Display(Name ="Photo Path")]
+        public string Photo_Path { get; set; }
+
+        [Display(Name = "Status")]
+        public bool Status{ get; set; }
+    }
+}
